141beb22394c3b1572e4e9b560b55b259a0aa6ea
Arsil <arsilsyahrowi@gmail.com>
2025-05-08 14:24:55 +0700

    commit pertama

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DataTable from './components/DataTable.vue';
+
+</script>
+
+<template>
+  <div class="p-8 bg-gray-100 min-h-screen">
+    <DataTable :items="items" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
